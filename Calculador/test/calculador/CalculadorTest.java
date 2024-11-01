f47fb39b3a483052cfbe461908897515aefb8446
adamulp <adamgoestoargentina@gmail.com>
2024-10-31 22:02:57 -0300

    JUnit v5.6.0

--- Calculador/test/calculador/CalculadorTest.java
+++ Calculador/test/calculador/CalculadorTest.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ JUnit version: 5.6.0 (Shift+F6 desde syscheck/JunitVersionCheck)
  */
 package calculador;
 
