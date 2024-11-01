7ff575fa387fb224379c0026d15ae71522f13c3b
adamulp <adamgoestoargentina@gmail.com>
2024-11-01 07:51:43 -0300

    Update Calculador.java
    Generic support for different number types

--- Calculador/src/calculador/Calculador.java
+++ Calculador/src/calculador/Calculador.java
@@ -1,20 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package calculador;
 
 /**
  *
- * @author adam
+ * @author Grupo16
  */
-public class Calculador {
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Calculador {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Calculador calc = new Calculador();
+        BigDecimal result1 = calc.sumar(5, 10);
+        BigDecimal result2 = calc.sumar(5.5, 4.5);
+        BigDecimal result3 = calc.sumar(new BigDecimal("10.5"), new BigDecimal("5.5"));
+
+        System.out.println("Sum (Integer): " + result1);
+        System.out.println("Sum (Double): " + result2);
+        System.out.println("Sum (BigDecimal): " + result3);
+
+        BigDecimal result4 = calc.restar(20.0, 3);
+        System.out.println("Difference: " + result4);
     }
-    
+
+    private BigDecimal toBigDecimal(Number number) {
+        if (number instanceof BigDecimal) {
+            return (BigDecimal) number;
+        } else if (number instanceof BigInteger) {
+            return new BigDecimal((BigInteger) number);
+        } else {
+            return BigDecimal.valueOf(number.doubleValue());
+        }
+    }
+
+    public BigDecimal sumar(Number a, Number b) {
+        return toBigDecimal(a).add(toBigDecimal(b));
+    }
+
+    public BigDecimal restar(Number a, Number b) {
+        return toBigDecimal(a).subtract(toBigDecimal(b));
+    }
+
+    public BigDecimal dividir(Number a, Number b) {
+        BigDecimal divisor = toBigDecimal(b);
+        if (divisor.compareTo(BigDecimal.ZERO) == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return toBigDecimal(a).divide(divisor);
+    }
+
+    public BigDecimal multiplicar(Number a, Number b) {
+        return toBigDecimal(a).multiply(toBigDecimal(b));
+    }
 }
+
+
