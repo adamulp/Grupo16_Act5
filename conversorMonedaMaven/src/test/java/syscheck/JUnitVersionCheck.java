b28f6e7952b0e90dea11dfd66483bfdfa6ee5a0b
adamulp <adamgoestoargentina@gmail.com>
2024-11-02 14:41:26 -0300

    Create JUnitVersionCheck.java

---
+++ conversorMonedaMaven/src/test/java/syscheck/JUnitVersionCheck.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions;
+
+public class JUnitVersionCheck {
+    public static void main(String[] args) {
+        String version = Assertions.class.getPackage().getImplementationVersion();
+        System.out.println("JUnit version: " + version);
+    }
+}
