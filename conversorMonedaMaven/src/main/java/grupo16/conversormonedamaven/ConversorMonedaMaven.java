ddf47473ff868c7bcc1b084a6ccf643ffb22b6e4
adamulp <adamgoestoargentina@gmail.com>
2024-11-02 14:17:56 -0300

    Start conversion de ANT a Maven

---
+++ conversorMonedaMaven/src/main/java/grupo16/conversormonedamaven/ConversorMonedaMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grupo16.conversormonedamaven;
+
+/**
+ *
+ * @author adam
+ */
+public class ConversorMonedaMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
