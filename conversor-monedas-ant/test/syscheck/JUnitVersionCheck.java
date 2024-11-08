abb6c75b5859a923cf7c9e2821a56e035957ad2a
adamulp <163217780+adamulp@users.noreply.github.com>
2024-11-08 16:26:39 -0300

    Delete conversor-monedas-ant directory

--- conversor-monedas-ant/test/syscheck/JUnitVersionCheck.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Assertions;
-
-public class JUnitVersionCheck {
-    public static void main(String[] args) {
-        String version = Assertions.class.getPackage().getImplementationVersion();
-        System.out.println("JUnit version: " + version);
-    }
-}
